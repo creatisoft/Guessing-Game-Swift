ecfde50c30596742c19edb4a0673c886ab46c921
Christopher Moya <chrism1389@gmail.com>
2024-06-17 21:47:43 -0700

    final update
    - this is the final update
    - have fun

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Guessing-Game-Swift
-a simple number guessing game written in swift
+
+## A simple number guessing game written in swift
+## version 0.1.0
+
+---------------------------------------
+## https://www.creatisoft.com
+## https://www.x.com/creatisoft
+---------------------------------------
+- Christopher M. 
+06-17-2024
--- Swift-Guessing-Game/Swift-Guessing-Game/main.swift
+++ Swift-Guessing-Game/Swift-Guessing-Game/main.swift
@@ -3,9 +3,51 @@
 //  Swift-Guessing-Game
 //
 //  Created by Christopher on 6/17/24.
+//  https://www.creatisoft.com
+//  https://github.com/creatisoft
 //
-
 import Foundation
 
-print("Hello, World!")
+//declare and initilize my variables
+var numberToGuess:Int = 0
+var gameIsRunning:Bool = true
 
+//I'll admit i don't understand the optional / forced unwrapping stuff just yet
+var userInput:String? = ""
+
+//assign a random generated number
+numberToGuess = Int.random(in: 1..<11)
+
+//ui / info
+print("Number Guessing Game written in Swift")
+print("=====================================")
+print("Guess a number between 1 and 10")
+print("=====================================")
+
+
+//while game loop is true
+while(gameIsRunning){
+    
+    //the answer to win
+    //print("answer key \(numberToGuess)")
+    
+    //ask user to input a number
+    print("Guess a number: ")
+    
+    //we read the user input here
+    userInput = readLine()
+    
+    //compare user input to integer
+    //if true exit the "game"
+    if(userInput == "\(numberToGuess)"){
+        
+        print("Correct! GameOver.")
+        gameIsRunning = false
+        
+    }else{
+        
+        //game is not over try again
+        print("Try again...")
+        
+    }
+}
